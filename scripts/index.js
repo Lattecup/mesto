8245ff2fca78a3fdc6afcf7d6c7239edc618fbe5
Anastasia Shchipakina <avshchipakina@yandex.ru>
2021-07-17 22:22:25 +0300

    feat: add script

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__input_text_type_title');
+let jobInput = formElement.querySelector('.form__input_text_type_subtitle');
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+
+  popupClose();
+}
+
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+formElement.addEventListener('submit', formSubmitHandler);
