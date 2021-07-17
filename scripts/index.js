7f88cfe779c10d3a868d1cf82d8c59f2cb05f478
Anastasia Shchipakina <avshchipakina@yandex.ru>
2021-07-17 23:05:54 +0300

    fix: change html script

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const closeButton = document.querySelector('.popup__close-button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.form__input_text_type_title');
-let jobInput = formElement.querySelector('.form__input_text_type_subtitle');
+let nameInput = formElement.querySelector('.form__input_type_title');
+let jobInput = formElement.querySelector('.form__input_type_subtitle');
 
 function popupOpen() {
   popup.classList.add('popup_opened');
