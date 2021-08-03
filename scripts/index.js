b68ac57796e95514810edb2658c0d3819b1e7ee0
Anastasia Shchipakina <avshchipakina@yandex.ru>
2021-08-03 16:33:58 +0300

    fix: corrections after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
-const popup = document.querySelector('.popup');
 const editProfilePopup = document.querySelector('.popup-edit');
 const addCardPopup = document.querySelector('.popup-add');
 const cardImagePopup = document.querySelector('.popup-image');
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
-const closeButton = document.querySelectorAll('.popup__close-button');
+const closeButtons = document.querySelectorAll('.popup__close-button');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.form-edit');
@@ -14,80 +13,71 @@ const cardFormElement = document.querySelector('.form-add');
 const placeInput = cardFormElement.querySelector('.form__input_type_place');
 const linkInput = cardFormElement.querySelector('.form__input_type_link');
 const cardTemplate = document.querySelector('#card-template');
+const cardContent = cardTemplate.content.querySelector('.card');
 const cards = document.querySelector('.cards');
 const imagePopup = document.querySelector('.popup__image');
 const imageCaptionPopup = document.querySelector('.popup__image-caption');
-const initialCards = [
-  {
-    name: 'Эльбрус',
-    link: './images/elements-elbrus.jpg'
-  },
-  {
-    name: 'Шары в Каппадокии',
-    link: './images/elements-kappadokiya.jpg'
-  },
-  {
-    name: 'Закат на Камчатке',
-    link: './images/elements-kamchatka.jpg'
-  },
-  {
-    name: 'Гималаи',
-    link: './images/elements-himalayas.jpg'
-  },
-  {
-    name: 'Маковое поле',
-    link: './images/elements-makovoe-pole.jpg'
-  },
-  {
-    name: 'Северное сияние в Исландии',
-    link: './images/elements-iceland-northen-lights.jpg'
-  }
-];
 
+// Создание карточки
 function createCard(element) {
-  const cardContent = cardTemplate.content.querySelector('.card');
   const card = cardContent.cloneNode(true);
   const cardImage = card.querySelector('.card__image');
   const cardCaption = card.querySelector('.card__caption');
 
   cardImage.src = element.link;
-  cardImage.alt = element.alt;
+  cardImage.alt = element.name;
   cardCaption.textContent = element.name;
 
   const likeButton = card.querySelector('.card__like-button');
   const removeButton = card.querySelector('.card__remove-button');
 
-  likeButton.addEventListener('click', (evt) => {
-    evt.target.classList.toggle('card__like-button_active');
-  });
-
-  removeButton.addEventListener('click', (evt) => {
-    evt.target.parentElement.remove();
-  });
-
-  cardImage.addEventListener('click', function() {
-    openImagePopup();
-
-    imagePopup.src = cardImage.src;
-    imagePopup.alt = cardImage.alt;
-    imageCaptionPopup.textContent = cardCaption.textContent;
-  });
+  likeButton.addEventListener('click', handleLikeIcon);
+  removeButton.addEventListener('click', handleRemoveCard);
+  cardImage.addEventListener('click', handlePreviewImage);
 
   return card;
 };
 
-initialCards.forEach(function (element) {
-  cards.append(createCard(element));
+// Поставить/удалить лайк 
+function handleLikeIcon (evt) {
+  evt.target.classList.toggle('card__like-button_active');
+};
+
+// Удаление карточки
+const handleRemoveCard = (evt) => {
+  evt.target.closest('.card').remove();
+};
+
+// Превью изображения
+function handlePreviewImage (evt) {
+  imagePopup.src = evt.target.src;
+  imagePopup.alt = evt.target.alt;
+  imageCaptionPopup.textContent = evt.target.alt;
+
+  openPopup(cardImagePopup);
+};
+
+// Добавление карточки в контейнер
+function renderCard(element, container) {
+  container.prepend(createCard(element));
+};
+
+// Заполнение страницы исходным массивом
+initialCards.forEach((element) => {
+  renderCard(element, cards);
 });
 
+// Закрытие попапа
 const openPopup = (popup) => {
   popup.classList.add('popup_opened');
 };
 
+// Открытие попапа
 const closePopup = (popup) => {
   popup.classList.remove('popup_opened');
 };
 
+// Открытие попапа редактирования профиля
 function openEditPopup() {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
@@ -95,14 +85,7 @@ function openEditPopup() {
   openPopup(editProfilePopup);
 };
 
-function openAddPopup() {
-  openPopup(addCardPopup);
-};
-
-function openImagePopup() {
-  openPopup(cardImagePopup);
-};
-
+// Редактирование данных профиля
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
@@ -112,21 +95,27 @@ function formSubmitHandler (evt) {
   closePopup(editProfilePopup);
 };
 
+// Добавление новой карточки
 function addFormSubmitHandler (evt) {
   evt.preventDefault();
 
-  cards.prepend(createCard({name: placeInput.value, link: linkInput.value}));
-
+  renderCard({name: placeInput.value, link: linkInput.value}, cards);
+  
   closePopup(addCardPopup);
+  cardFormElement.reset();
 };
 
-closeButton.forEach((item) => {
-  item.addEventListener('click', (evt) => {
-    evt.target.closest('.popup').classList.remove('popup_opened');
+// Закрытие всех попапов
+closeButtons.forEach((item) => {
+  const popup = item.closest('.popup');
+  item.addEventListener('click', function() {
+    closePopup(popup);
   });
 });
 
 editButton.addEventListener('click', openEditPopup);
-addButton.addEventListener('click', openAddPopup)
+addButton.addEventListener('click', function() {
+  openPopup(addCardPopup);
+});
 formElement.addEventListener('submit', formSubmitHandler);
 cardFormElement.addEventListener('submit', addFormSubmitHandler);
---
+++ scripts/initial-cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Эльбрус',
+    link: './images/elements-elbrus.jpg'
+  },
+  {
+    name: 'Шары в Каппадокии',
+    link: './images/elements-kappadokiya.jpg'
+  },
+  {
+    name: 'Закат на Камчатке',
+    link: './images/elements-kamchatka.jpg'
+  },
+  {
+    name: 'Гималаи',
+    link: './images/elements-himalayas.jpg'
+  },
+  {
+    name: 'Маковое поле',
+    link: './images/elements-makovoe-pole.jpg'
+  },
+  {
+    name: 'Северное сияние в Исландии',
+    link: './images/elements-iceland-northen-lights.jpg'
+  }
+];
